a5da6f5b30b49a9216988ed93b7c0ff60a20319f
Ardiant42 <lulukardianto42@gmail.com>
2024-06-04 10:14:22 +0700

    menambahkan login

--- src/firebase.jsx
+++ src/firebase.jsx
@@ -7,14 +7,13 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_KEY,
-  authDomain: "store-tutorial-138ba.firebaseapp.com",
-  projectId: "store-tutorial-138ba",
-  storageBucket: "store-tutorial-138ba.appspot.com",
-  messagingSenderId: "67926225197",
-  appId: "1:67926225197:web:6979ce478588db2d0a23a4"
+  authDomain: "store-2b84b.firebaseapp.com",
+  projectId: "store-2b84b",
+  storageBucket: "store-2b84b.appspot.com",
+  messagingSenderId: "222861520020",
+  appId: "1:222861520020:web:390369fc16ce7a615cd881"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-export const auth = getAuth(app);
+export const auth = getAuth();
